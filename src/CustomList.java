10568d85a5c3e6cc694d3d5290e87ece3f6c4a78
yusufgoksu <goksuy@mef.edu.tr>
2024-12-16 16:13:16 +0300

    CustomList Added

---
+++ src/CustomList.java
@@ -0,0 +1,82 @@
+import java.util.AbstractList;
+
+public class CustomList<T> extends AbstractList<T> {
+    private Object[] elements;
+    private int size;
+
+    public CustomList() {
+        elements = new Object[10];
+        size = 0;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        return (T) elements[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public T set(int index, T element) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        T old = (T) elements[index];
+        elements[index] = element;
+        return old;
+    }
+
+    @Override
+    public void add(int index, T element) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        if (size == elements.length) {
+            resize();
+        }
+        for (int i = size; i > index; i--) {
+            elements[i] = elements[i - 1];
+        }
+        elements[index] = element;
+        size++;
+    }
+
+    @Override
+    public T remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        T removed = (T) elements[index];
+        for (int i = index; i < size - 1; i++) {
+            elements[i] = elements[i + 1];
+        }
+        elements[--size] = null;
+        return removed;
+    }
+
+    private void resize() {
+        Object[] newElements = new Object[elements.length * 2];
+        System.arraycopy(elements, 0, newElements, 0, elements.length);
+        elements = newElements;
+    }
+    public static String join(String separator, CustomList<String> list) {
+        if (list.size() == 0) {
+            return "";
+        }
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < list.size(); i++) {
+            result.append(list.get(i));
+            if (i < list.size() - 1) {
+                result.append(separator);
+            }
+        }
+        return result.toString();
+    }
+
+}
